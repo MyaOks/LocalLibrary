acd9a2c80db4b2cea472613432786f3633131fe7
MyaOks <oksana.myasnikova.2003@mail.ru>
2023-10-14 13:47:44 +0700

    Часть 10

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hmoc$wi&e))aj-qj(oakp!)7!*on&o(&dh4hfbn^%ufvvb!b6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -53,7 +54,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'locallibrary.urls'
 
-import os
 
 TEMPLATES = [
     {
